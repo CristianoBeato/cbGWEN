fd1818dd73054b44438dc64a17d9fd141c4a66cb
Beato <cristianobeato_dm@hotmail.com>
2025-08-30 10:43:07 -0300

    implementation on SDL3 platform

--- gwen/include/Gwen/Platform/SDL3Platform.h
+++ gwen/include/Gwen/Platform/SDL3Platform.h
@@ -27,8 +27,8 @@
 ============================================================================================
 */
 
-#ifndef GWEN_PLATFORM_H
-#define GWEN_PLATFORM_H
+#ifndef GWEN_SDL_PLATFORM_H
+#define GWEN_SDL_PLATFORM_H
 
 #include "Gwen/Structures.h"
 #include "Gwen/Events.h"
@@ -37,29 +37,47 @@ namespace Gwen
 {
 	namespace Platform
 	{
-		class GWEN_EXPORT Base
+		class SDL3Window
+		{
+		public:
+			SDL3Window( const uint32_t in_width, const uint32_t in_height, const char* in_title );
+			~SDL3Window( void );
+			bool	Create( const uint32_t in_flags );
+			void	Maximize( void );
+			void	Minimize( void );
+			void	Restore( void );
+			bool	HasFocus( void ) const;
+
+		private:
+			const char*			m_title;
+			uint32_t			m_width;
+			uint32_t			m_height;
+			struct SDL_Window*	m_window;
+		};
+
+		class GWEN_EXPORT SDL3 : public Platform::Base
 		{
 			public:
-				Base( void );
-				virtual ~Base( void );
+				SDL3( void );
+				virtual ~SDL3( void );
 				
 				/// @brief Do nothing for this many milliseconds
 				/// @param in_ms to wait 
-				virtual void Sleep( const uint32_t in_ms ) {};
+				virtual void Sleep( const uint32_t in_ms ) override;
 
 				/// @brief Set the system cursor to iCursor
 				/// Cursors are defined in Structures.h
 				/// @param in_cursorID cursor indetity
-				virtual void SetCursor( const uint8_t in_cursorID ) {};
+				virtual void SetCursor( const uint8_t in_cursorID ) override;
 
 				/// @brief Get the relative cursor position
 				/// @param p position of the cursos
-				virtual void GetCursorPos( Gwen::Point & p ) {};
+				virtual void GetCursorPos( Gwen::Point & p ) override;
 				
 				/// @brief get the size of the current desktop composition 
 				/// @param w desktop Width 
 				/// @param h desktop height
-				virtual void GetDesktopSize( int & w, int & h ) {};
+				virtual void GetDesktopSize( int & w, int & h ) override;
 
 				//
 				// Used by copy/paste
@@ -67,7 +85,7 @@ namespace Gwen
 
 				/// @brief aquire the clipboard string content  
 				/// @return the clipboard content 
-				virtual UnicodeString GetClipboardText( void ) {};
+				virtual UnicodeString GetClipboardText( void );
 
 				/// @brief Set the clipboard text 
 				/// @param str 
@@ -76,26 +94,27 @@ namespace Gwen
 
 				/// @brief Needed for things like double click
 				/// @return 
-				virtual float GetTimeInSeconds( void ) {};
+				virtual float GetTimeInSeconds( void ) { return 0.0f; };
 
 				//
 				// System Dialogs ( Can return false if unhandled )
 				//
-				virtual bool FileOpen( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback );
-				virtual bool FileSave( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback );
-				virtual bool FolderOpen( const String & Name, const String & StartPath, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback );
+				virtual bool FileOpen( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback ) { return false };
+				virtual bool FileSave( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback ) { return false };
+				virtual bool FolderOpen( const String & Name, const String & StartPath, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback ) { return false };
 
 				//
 				// Window Creation
 				//
-				virtual void* CreatePlatformWindow( int x, int y, int w, int h, const Gwen::String & strWindowTitle );
-				virtual void DestroyPlatformWindow( void* pPtr );
-				virtual void SetBoundsPlatformWindow( void* pPtr, int x, int y, int w, int h );
-				virtual void MessagePump( void* pWindow, Gwen::Controls::Canvas* ptarget );
-				virtual bool HasFocusPlatformWindow( void* pPtr );
-				virtual void SetWindowMaximized( void* pPtr, bool bMaximized, Gwen::Point & pNewPos, Gwen::Point & pNewSize );
-				virtual void SetWindowMinimized( void* pPtr, bool bMinimized );
+				virtual void* CreatePlatformWindow( int x, int y, int w, int h, const Gwen::String & strWindowTitle ) { return nullptr; }
+				virtual void DestroyPlatformWindow( void* pPtr ) {}
+				virtual void SetBoundsPlatformWindow( void* pPtr, int x, int y, int w, int h ) { }
+				virtual void MessagePump( void* pWindow, Gwen::Controls::Canvas* ptarget ) { }
+				virtual bool HasFocusPlatformWindow( void* pPtr ) { return false; }
+				virtual void SetWindowMaximized( void* pPtr, bool bMaximized, Gwen::Point & pNewPos, Gwen::Point & pNewSize ) {}
+				virtual void SetWindowMinimized( void* pPtr, bool bMinimized ) {}
 		};
 	}
 }
-#endif
+
+#endif // !GWEN_SDL_PLATFORM_H
--- gwen/src/Platforms/SDL3Platform.cpp
+++ gwen/src/Platforms/SDL3Platform.cpp
@@ -28,4 +28,224 @@
 */
 
 #include "Precompiled.hpp"
+#include "Gwen/Platform/SDL3Platform.h"
 
+#include <SDL3/SDL_timer.h>
+#include <SDL3/SDL_mouse.h>
+#include <SDL3/SDL_clipboard.h>
+#include <SDL3/SDL_video.h>
+#include "SDL3Platform.h"
+
+Gwen::Platform::SDL3::SDL3( void )
+{
+
+}
+
+Gwen::Platform::SDL3::~SDL3( void )
+{
+
+}
+
+void Gwen::Platform::SDL3::Sleep( const uint32_t in_ms )
+{
+	// put thread to sleep
+	SDL_Delay( in_ms );
+}
+
+static SDL_Cursor* cursor = nullptr;
+void Gwen::Platform::SDL3::SetCursor( const uint8_t in_cursorID )
+{
+	SDL_Cursor* old = nullptr;
+	SDL_SystemCursor syscur = SDL_SYSTEM_CURSOR_DEFAULT;
+
+	switch ( in_cursorID )
+	{
+	case Normal:
+		syscur = SDL_SYSTEM_CURSOR_DEFAULT;
+		break;
+
+	case Beam:
+		syscur = SDL_SYSTEM_CURSOR_TEXT;
+		break;
+
+	case SizeNS:
+		syscur = SDL_SYSTEM_CURSOR_NS_RESIZE;
+		break;
+
+	case SizeWE:
+		syscur = SDL_SYSTEM_CURSOR_EW_RESIZE;
+		break;
+
+	case SizeNWSE:
+		syscur = SDL_SYSTEM_CURSOR_NESW_RESIZE;
+		break;
+
+	case SizeNESW:
+		syscur = SDL_SYSTEM_CURSOR_NWSE_RESIZE;
+		break;
+
+	case SizeAll:
+		syscur = SDL_SYSTEM_CURSOR_MOVE;
+		break;
+
+	case No:
+		syscur = SDL_SYSTEM_CURSOR_NOT_ALLOWED;
+		break;
+
+	case Wait:
+		syscur = SDL_SYSTEM_CURSOR_WAIT;
+		break;
+
+	case Finger:
+		syscur = SDL_SYSTEM_CURSOR_POINTER;
+		break;
+
+	default:
+		syscur = SDL_SYSTEM_CURSOR_DEFAULT;
+		break;
+	}
+
+	// store current cursor
+	old = cursor;
+
+	// create a new pointer
+	cursor = SDL_CreateSystemCursor( syscur );
+
+	// set the cursor
+	SDL_SetCursor( cursor );
+
+	// release the previous cursor pointer
+	if ( old != nullptr )
+		SDL_DestroyCursor( old );
+}
+
+//TODO: 
+void Gwen::Platform::SDL3::GetDesktopSize( int & w, int & h )
+{
+
+}
+
+Gwen::UnicodeString Gwen::Platform::SDL3::GetClipboardText( void )
+{
+	Gwen::TextObject inString;
+	if ( !SDL_HasClipboardText() )
+		return Gwen::UnicodeString();
+
+	// aquire the clipboard
+	inString = SDL_GetClipboardText();
+	
+	return inString.GetUnicode();
+}
+
+bool Gwen::Platform::SDL3::SetClipboardText( const UnicodeString & str )
+{
+	Gwen::TextObject outString = str;
+	
+	// clear the clipboard before we set a new content 
+	SDL_ClearClipboardData();
+
+	return SDL_SetClipboardText( outString.c_str() );
+}
+
+float Gwen::Platform::SDL3::GetTimeInSeconds( void )
+{
+	uint64_t time = SDL_GetTicks();
+	return time / 1000;
+}
+
+bool Gwen::Platform::SDL3::FileOpen( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback )
+{
+	return false; //TODO: SDL3 now suport file open Dialog
+}
+
+bool Gwen::Platform::SDL3::FileSave( const String & Name, const String & StartPath, const String & Extension, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback )
+{
+	return false; //TODO: SDL3 now suport file open Dialog
+}
+
+bool Gwen::Platform::SDL3::FolderOpen( const String & Name, const String & StartPath, Gwen::Event::Handler* pHandler, Event::Handler::FunctionWithInformation fnCallback )
+{
+	return false; //TODO: SDL3 now suport file open Dialog
+}
+
+void* Gwen::Platform::SDL3::CreatePlatformWindow( int x, int y, int w, int h, const Gwen::String & strWindowTitle )
+{
+	SDL3Window* newWindow = new SDL3Window();
+	reinterpret_cast<void*>( newWindow );
+}
+
+void Gwen::Platform::SDL3::DestroyPlatformWindow( void* pPtr )
+{
+	SDL3Window* window = static_cast<SDL3Window*>( pPtr );
+	delete window;
+}
+
+void Gwen::Platform::SDL3::SetBoundsPlatformWindow( void* pPtr, int x, int y, int w, int h )
+{
+	SDL3Window* window = static_cast<SDL3Window*>( pPtr );
+	// TODO:
+}
+
+void Gwen::Platform::SDL3::MessagePump( void* pWindow, Gwen::Controls::Canvas* ptarget )
+{
+	// TODO:
+}
+
+bool Gwen::Platform::SDL3::HasFocusPlatformWindow( void* pPtr )
+{
+	SDL3Window* window = static_cast<SDL3Window*>( pPtr );
+	return window->HasFocus();
+}
+
+void Gwen::Platform::SDL3::SetWindowMaximized( void* pPtr, bool bMaximized, Gwen::Point & pNewPos, Gwen::Point & pNewSize )
+{
+	SDL3Window* window = static_cast<SDL3Window*>( pPtr );
+	if ( bMaximized )
+		window->Maximize();
+	else
+		window->Restore();
+}
+
+void Gwen::Platform::SDL3::SetWindowMinimized( void* pPtr, bool bMinimized )
+{
+	SDL3Window* window = static_cast<SDL3Window*>( pPtr );
+	if ( bMinimized )
+		window->Minimize();
+	else
+		window->Restore();	
+}
+
+Gwen::Platform::SDL3Window::SDL3Window(const uint32_t in_width, const uint32_t in_height, const char *in_title ) : 
+	m_width( in_width ),
+	m_height( in_height ),
+	m_title( in_title ),
+	m_window( nullptr )
+{
+}
+
+bool Gwen::Platform::SDL3Window::Create( const uint32_t in_flags)
+{
+	m_window = SDL_CreateWindow( m_title, m_width, m_height, in_flags );
+    return m_window != nullptr;;
+}
+
+void Gwen::Platform::SDL3Window::Maximize(void)
+{
+	SDL_MaximizeWindow( m_window );
+}
+
+void Gwen::Platform::SDL3Window::Minimize(void)
+{
+	SDL_MinimizeWindow( m_window );
+}
+
+void Gwen::Platform::SDL3Window::Restore(void)
+{
+	SDL_RestoreWindow( m_window );
+}
+
+bool Gwen::Platform::SDL3Window::HasFocus(void) const
+{
+	SDL_WindowFlags flags = SDL_GetWindowFlags( m_window );
+	return flags & SDL_WINDOW_INPUT_FOCUS;
+}
